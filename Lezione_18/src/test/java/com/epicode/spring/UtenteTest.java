abecb5de1518d0832671377d2296cfb25d6ff568
Daviduxx <borotto.davide@gmail.com>
2023-08-02 16:15:42 +0200

    classe di test

---
+++ Lezione_18/src/test/java/com/epicode/spring/UtenteTest.java
@@ -0,0 +1,34 @@
+package com.epicode.spring;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UtenteTest {
+
+	@Test
+	void testReturnAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testReturnOne() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCreaUtente() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRimuoviUtente() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAggiornaUtente() {
+		fail("Not yet implemented");
+	}
+
+}
