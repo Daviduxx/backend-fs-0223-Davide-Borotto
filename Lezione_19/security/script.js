5ce276ffdfeb69e7f381c09bf0085bb6209897a5
Daviduxx <borotto.davide@gmail.com>
2023-08-04 00:52:23 +0200

    errore cors

--- Lezione_19/security/script.js
+++ Lezione_19/security/script.js
@@ -3,25 +3,27 @@ const name = document.getElementById("name");
 const pw = document.getElementById("password");
 const user = {};
 
-const ENDPOINT = "http://localhost:8080/api/auth/login";
-const HEADER = {
-    method: 'POST',
-    body: JSON.stringify(user),
-    headers: {
-        'Content-type': 'application/json'
-    }
-};
+const ENDPOINT = "http://localhost:8080/api/auth/";
  
 loginBtn.addEventListener("click", (e) => {
 
     e.preventDefault();
 
-   user.username = name;
-   user.password = pw;
+   user.username = name.value;
+   user.password = pw.value;
+   user.email = "pippo.baudo@example.it";
+   user.name = "pippo";
+   user.lastname = "baudo";
 
     console.log(user);
 
-    fetch(ENDPOINT, HEADER)
+    fetch(ENDPOINT, {
+        method: 'POST',
+        body: JSON.stringify(user),
+        headers: {
+            'Content-type': 'application/json'
+        }
+    })
     .then(res => console.log(res))
 
 })
