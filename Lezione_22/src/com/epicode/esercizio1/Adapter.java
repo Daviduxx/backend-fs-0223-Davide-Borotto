7b41bb2f926acf777b5d5c6534e800f2d2b59fa1
Daviduxx <borotto.davide@gmail.com>
2023-08-24 21:15:47 +0200

    esercizio1

---
+++ Lezione_22/src/com/epicode/esercizio1/Adapter.java
@@ -0,0 +1,30 @@
+package com.epicode.esercizio1;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
+
+public class Adapter implements DataSource {
+	
+	private Info info;
+
+	public Adapter(Info info) {
+		this.info = info;
+	}
+
+	@Override
+	public String getNomeCompleto() {
+			return this.info.getNome() + this.info.getCognome();
+	}
+
+	@Override
+	public int getEta() {
+		
+		LocalDate dataNascita = this.info.getDataDiNascita().toInstant()
+				.atZone(ZoneId.systemDefault()).toLocalDate();
+		LocalDate adesso = LocalDate.now();
+		
+		return Period.between(adesso, dataNascita).getYears();
+	}
+
+}
