e0bb833977bcec39470517133cfbcd535bec27e3
Daviduxx <borotto.davide@gmail.com>
2023-08-25 13:19:22 +0200

    initial commit

---
+++ Esercitazione6/src/main/java/com/epicode/spring/Esercitazione6Application.java
@@ -0,0 +1,13 @@
+package com.epicode.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Esercitazione6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Esercitazione6Application.class, args);
+	}
+
+}
